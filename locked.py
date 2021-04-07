d1fec9e0abe3d01b81228709c7dff02aeaed8707
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-04-07 14:28:58 +0700

    Update locked.py

--- locked.py
+++ locked.py
@@ -3,7 +3,7 @@ import sys
 if sys.version[0:3] != "3.9":
   sys.exit("[+] anda harus menggunakan versi python 3.9, versi python anda sekarang : "+sys.version[0:3])
 
-if __name__ == "__main__":
+if __name__=="__main__": 
   try:
     __import__("locked").main()
   except Exception as E:
